5bba9fae85a43e62c67028e170d2427fad1a7902
Leandro Rodriguez <leandro_driguez@outlook.com>
2025-02-21 22:13:32 -0500

    0058-LENGTH-OF-LAST-WORD Stats: Time: 0 ms (100%), Space: 8.9 MB (31.87%) - LeetHub

--- 0058-length-of-last-word/0058-length-of-last-word.cpp
+++ 0058-length-of-last-word/0058-length-of-last-word.cpp
@@ -1,18 +1,18 @@
 class Solution {
 public:
     int lengthOfLastWord(string s) {
-        string currWord = "";
+        int ptr1, ptr2;
         for (int i = 0; i < s.size(); i++) {
             if (s[i] == ' ') {
                 while (i < s.size() && s[i] == ' ') {i++;}
                 i--;
             }
             else {
-                currWord = "";
-                while (i < s.size() && s[i] != ' ') { currWord += string(1, s[i++]); }
+                ptr1 = i; ptr2 = i;
+                while (i < s.size() && s[i] != ' ') { ptr2=i; i++; }
                 i--;
             }
         }
-        return currWord.size();
+        return ptr2-ptr1+1;
     }
 };
