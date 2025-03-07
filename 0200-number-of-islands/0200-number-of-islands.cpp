b04448de204cd5c8d5f5eda028e66f0fe7b14ae9
Leandro Rodriguez <leandro_driguez@outlook.com>
2025-03-06 21:28:20 -0500

    0200-NUMBER-OF-ISLANDS Stats: Time: 35 ms (21.37%), Space: 23.4 MB (29.18%) - LeetHub

--- 0200-number-of-islands/0200-number-of-islands.cpp
+++ 0200-number-of-islands/0200-number-of-islands.cpp
@@ -1,39 +1,46 @@
 class Solution {
-public:
-    int numIslands(vector<vector<char>>& grid) {
-        int n = grid.size(), m = grid[0].size();
-        
-        int vX[4] = {0, 1, 0, -1};
-        int vY[4] = {-1, 0, 1, 0};
-
+private:
+    void discoverIsland(vector<vector<char>>& grid, int r, int c) {
         queue<pair<int,int>> q;
+        q.push({r,c});
 
-        int amountIslands = 0;
-        for (int i = 0; i < n; i++) {
-            for (int j = 0; j < m; j++) {
+        int vR[] = {1, 0, -1, 0};
+        int vC[] = {0, 1, 0, -1};
+        
+        while (!q.empty()) {
+            auto cell = q.front();
+            q.pop();
+            
+            if (grid[cell.first][cell.second] == '-')
+                continue;
+            grid[cell.first][cell.second] = '-';
+
+            for (int d = 0, i, j; d < 4; d++) {
+                i = cell.first + vR[d];
+                j = cell.second + vC[d];
+
+                if (i < 0 || j < 0 || i >= grid.size() || j >= grid[0].size())
+                    continue;
+                
                 if (grid[i][j] == '1') {
-                    amountIslands++;
-
-                    grid[i][j] = '0';
                     q.push({i,j});
-
-                    while (!q.empty()) {
-                        auto cell = q.front();
-                        q.pop();
-
-                        for (int d = 0, r, c; d < 4; d++) {
-                            r = cell.first + vY[d];
-                            c = cell.second + vX[d];
-
-                            if (r >= 0 && r < n && c >= 0 && c < m && grid[r][c] == '1') {
-                                grid[r][c] = '0';
-                                q.push({r,c});
-                            }
-                        }
-                    }
                 }
             }
         }
+    }
+public:
+    int numIslands(vector<vector<char>>& grid) {
+        int amountIslands = 0;
+
+        for (int i = 0; i < grid.size(); i++) {
+            for (int j = 0; j < grid[0].size(); j++) {
+                if (grid[i][j] == '1') {
+                    amountIslands++;
+                    discoverIsland(grid, i, j);
+                }
+            }
+        }
+
         return amountIslands;
     }
 };
