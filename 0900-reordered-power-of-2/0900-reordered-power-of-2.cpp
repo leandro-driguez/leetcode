f7090d062fa259de539c111d2424ade937db8ab1
Leandro Rodriguez <leandro_driguez@outlook.com>
2025-08-09 23:05:30 -0400

    0900-REORDERED-POWER-OF-2 Stats: Time: 2 ms (34.81%), Space: 9.4 MB (27.14%) - LeetHub

--- 0900-reordered-power-of-2/0900-reordered-power-of-2.cpp
+++ 0900-reordered-power-of-2/0900-reordered-power-of-2.cpp
@@ -1,38 +1,20 @@
 class Solution {
 private:
     string convertToSortedString(int n) {
-        vector<int> freq(10, 0);
-        while (n > 0) {
-            int dig = n % 10;
-            n = n / 10;
-            freq[dig]++;
-        }
-        string res = "";
-        for (int i = 0; i < 10; i++) {
-            if (freq[i] > 0) {
-                char c = '0' + i;
-                string tmp;
-                tmp = c;
-
-                while (freq[i] > 0) {
-                    res += tmp;
-                    freq[i]--;
-                }
-            }
-        }
-        return res;
+        string s = to_string(n);
+        sort(s.begin(), s.end());
+        return s;
     }
 public:
     bool reorderedPowerOf2(int n) {
-        unordered_set<string> hashSet;
-        for (int i = 1; i < 1e9;) {
-            string s = convertToSortedString(i);
-            hashSet.insert(s);
-            i *= 2;
+        string n_str = convertToSortedString(n);
+        
+        for (int i = 0; i < 31; i++) {
+            int power = 1<<i;
+            if (convertToSortedString(power) == n_str)
+                return true;
         }
 
-        string n_str = convertToSortedString(n);
-
-        return hashSet.find(n_str) != hashSet.end();
+        return false;
     }
 };
